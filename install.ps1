1ea2198f45ccd6408e3347183751819e2b160d61
Bigsby <5595360+bigsby-exe@users.noreply.github.com>
2024-11-19 22:41:17 +0000

    updated to use synced folder such as onedrive

--- install.ps1
+++ install.ps1
@@ -4,32 +4,43 @@ if(!($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Adminis
     break
 }
 
-$sourceFolder = Read-Host "Enter source"
-$destinationFolder = "C:\ps"
+# $sourceFolder = Read-Host "Enter source "
+write-Host "Enter location of PowerShell files and profiles (e.g. OneDrive folder)"
+write-host "This needs to have a profile.ps1 at the root level"
+$destinationFolder = Read-Host "Location"
 
-Robocopy $sourceFolder $destinationFolder /MIR /FFT /Z /W:5
+$Modules_source = Join-Path -Path $folderPath -ChildPath "\Modules"
+$Modules_destination = "C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
+
+Robocopy $Modules_source $Modules_destination /MIR /FFT /Z /W:5
 
 $taskName = "SyncFolders"
-$action = New-ScheduledTaskAction -Execute "robocopy" -Argument "`"$sourceFolder`" `"$destinationFolder`" /MIR /FFT /Z /W:5"
+$action = New-ScheduledTaskAction -Execute "robocopy" -Argument "`"$Modules_source`" `"$Modules_destination`" /MIR /FFT /Z /W:5"
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Description "Sync folders using Robocopy"
 
 $profileFilePath = $PROFILE.AllUsersAllHosts
-$lineToAdd = '. C:\ps\profile.ps1'
+
+if (!(Test-Path -Path $destinationFolder)) {
+    New-Item -Path $destinationFolder -ItemType Directory
+}
+$similprofile = Join-Path -Path $folderPath -ChildPath "profile.ps1"
+
+$lineToAdd = ". $similprofile"
 if (Test-Path $profileFilePath) {
     Add-Content -Path $profileFilePath -Value $lineToAdd
 } else {
     $lineToAdd | Set-Content -Path $profileFilePath
 }
 
-$profileScriptPath = "C:\ps\profile.ps1"
-$lineToAdd = 'Write-Host "It works!"'
-if (!(Test-Path $profileScriptPath)) {
-    $lineToAdd | Set-Content -Path $profileScriptPath
-}
+#$profileScriptPath = "C:\ps\profile.ps1"
+#$lineToAdd = 'Write-Host "It works!"'
+#if (!(Test-Path $profileScriptPath)) {
+#    $lineToAdd | Set-Content -Path $profileScriptPath
+#}
 
-New-Item -ItemType Directory -Path C:\ps\Modules -Force
-$key = (Get-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager').OpenSubKey('Environment', $true)
-$path = $key.GetValue('PSModulePath','','DoNotExpandEnvironmentNames')
-$path += ';C:\ps\Modules'
-$key.SetValue('PSModulePath',$path,[Microsoft.Win32.RegistryValueKind]::ExpandString)
+#New-Item -ItemType Directory -Path C:\ps\Modules -Force
+#$key = (Get-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager').OpenSubKey('Environment', $true)
+#$path = $key.GetValue('PSModulePath','','DoNotExpandEnvironmentNames')
+#$path += ';C:\ps\Modules'
+#$key.SetValue('PSModulePath',$path,[Microsoft.Win32.RegistryValueKind]::ExpandString)
